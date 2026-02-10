9f2eb3ea4bf441c462f00da5837d6677c7842257
OXANA38 <bars22-79@mail.ru>
2026-02-10 18:04:28 +0300

    Create config.py

---
+++ bot/config.py
@@ -0,0 +1,11 @@
+# bot/config.py
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    BOT_TOKEN: str
+    ADMIN_IDS: list[int] = []
+    
+    class Config:
+        env_file = ".env"
+
+config = Settings()
